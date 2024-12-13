2b71b4b4b4ef1d00dae78dc1a5e7a743f5936e67
matsuda-tkm <matsu20010403@gmail.com>
2024-12-14 01:48:00 +0900

    multilingual-e5-smallによる埋め込み

--- experiments/src/embedding/__init__.py
+++ experiments/src/embedding/__init__.py
@@ -1,6 +1,7 @@
 # src/embedding/__init__.py
 
 from .base import BaseEmbedder
+from .e5_embedder import E5Embedder
 from .gemini_embedder import GeminiEmbedder
 
-__all__ = ["BaseEmbedder", "GeminiEmbedder"]
+__all__ = ["BaseEmbedder", "GeminiEmbedder", "E5Embedder"]
